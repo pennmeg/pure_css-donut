3aad75a6f48c43f3953f9ecef25704a233ff88b0
pennmeg <penn.meg@gmail.com>
2018-11-14 15:22:03 -0500

    Sprinkles fall from above

--- script-sprinkles.js
+++ script-sprinkles.js
@@ -8,8 +8,12 @@ document.addEventListener("DOMContentLoaded", function() {
     var sprinkles = document.getElementsByClassName("sprinkle");
     // console.log("All sprinkles: ", sprinkles);
     for (var i = 0; i < sprinkles.length; i++) {
-      sprinkles[i].style.display = "block";
+      // ---- Have sprinkles appear using display:block;
+      // sprinkles[i].style.display = "block";
       // console.log("--- Sprinkles should be visible ---");
+      // --- Decided to practice CSS Animations and have sprinkles fall from above
+      sprinkles[i].classList.add("s--" + [i+1]);
+      // console.log("--- Sprinkles should fall in ---");
     }
   };
 });
