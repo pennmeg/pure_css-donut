b8b316c0a24ebe8403355a37adb4675f9c64bafa
pennmeg <penn.meg@gmail.com>
2018-11-14 15:26:05 -0500

    fixed typo in some colors

--- script-sprinkles.js
+++ script-sprinkles.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
   sprinkleButton.addEventListener("click", addSprinkles);
   function addSprinkles(){
     // console.log("=== sprinkleButton ===");
-    console.log("--- Add the Sprinkles! ---");
+    // console.log("--- Add the Sprinkles! ---");
     var sprinkles = document.getElementsByClassName("sprinkle");
     // console.log("All sprinkles: ", sprinkles);
     for (var i = 0; i < sprinkles.length; i++) {
